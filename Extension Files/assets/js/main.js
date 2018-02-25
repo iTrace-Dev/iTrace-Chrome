4cce0d76de486fe0edd16749d1af7f8d89834687
Joey <jmiller2398@gmail.com>
2018-02-25 01:43:25 -0500

    Initial commit, adding basic files for chrome extension

---
+++ Extension Files/assets/js/main.js
@@ -0,0 +1,26 @@
+// main JavaScript driver for the iTrace-Chrome plugin, all data will be handled here
+
+/**
+ * Get the current URL.
+ *
+ * @param {function(string)} callback called when the URL of the current tab
+ *   is found.
+ */
+function getCurrentTabUrl(callback) {
+  // Query filter to be passed to chrome.tabs.query - see
+  // https://developer.chrome.com/extensions/tabs#method-query
+  var queryInfo = {
+    active: true,
+    currentWindow: true
+  };
+
+  chrome.tabs.query(queryInfo, (tabs) => {
+    var tab = tabs[0];
+
+    var url = tab.url;
+
+    console.assert(typeof url == 'string', 'tab.url should be a string');
+
+    callback(url);
+  });
+}
