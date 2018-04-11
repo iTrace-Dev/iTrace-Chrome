b994e41337e20f1b0cfb0b6c16c1d684495ff39b
Joey <jmiller2398@gmail.com>
2018-04-11 00:43:33 -0400

    StackOverflow now works! Currently logs to console xml writing to follow

--- Extension Files/assets/js/getSOCoordinate.js
+++ Extension Files/assets/js/getSOCoordinate.js
@@ -1,11 +1,128 @@
 // Listen for messages
 chrome.runtime.onMessage.addListener(function (msg, sender, sendResponse) {
     // If the received message has the expected format...
-    if (msg.text === 'get_so_coordinate') {
+    /*if (msg.text === 'get_so_coordinate') {
         // Call the specified callback, passing
         // the web-page's DOM content as argument
         var elements = document.elementsFromPoint(msg.x, msg.y);
-        console.log(elements);
-        sendResponse(elements);
+        var stringElements = [];
+        elements.forEach(function (element) {
+            var index;
+            if (element.parentElement) {
+                for (var i = 0; i < element.parentElement.children.length; i++) {
+                    if (element.parentElement.children[i] == element) {
+                        index = i;
+                    }
+                }
+                stringElements.push({ element: element.parentElement.innerHTML, index: index });
+            } else {
+                stringElements.push({ element: element.innerHTML, index: 0 });
+            }
+        });
+        //console.log(stringElements);
+        sendResponse(stringElements);
+    }*/
+
+    var questionElement = document.getElementById('question');
+    var answerElements = document.getElementById('answers').children;
+    var elements = document.elementsFromPoint(msg.x, msg.y);
+
+    var sentResult = false;
+    for (element of elements) {
+        if (element) {
+            if (element.tagName == 'CODE') {
+                if (questionElement.contains(element)) {
+                    // question code
+                    console.log('question code');
+                    sentResult = true;
+                    sendResponse({ result: 'question code', x: msg.x, y: msg.y });
+                    return;
+                }
+                for (answer of answerElements) {
+                    if (answer.contains(element)) {
+                        // answer code
+                        console.log('answer code');
+                        sentResult = true;
+                        sendResponse({ result: 'answer code', x: msg.x, y: msg.y });
+                        return;
+                    }
+                }
+            }
+    
+            if (element.tagName == 'IMG') {
+                if (questionElement.contains(element)) {
+                    // question code
+                    console.log('question image');
+                    sentResult = true;
+                    sendResponse({ result: 'question image', x: msg.x, y: msg.y });
+                    return;
+                }
+                for (answer of answerElements) {
+                    if (answer.contains(element)) {
+                        // answer code
+                        console.log('answer image');
+                        sentResult = true;
+                        sendResponse({ result: 'answer image', x: msg.x, y: msg.y });
+                        return;
+                    }
+                }
+            }
+    
+            if (element.classList.contains('post-text')) {
+                if (questionElement.contains(element)) {
+                    // question code
+                    console.log('question text');
+                    sentResult = true;
+                    sendResponse({ result: 'question text', x: msg.x, y: msg.y });
+                    return;
+                }
+                for (answer of answerElements) {
+                    if (answer.contains(element)) {
+                        // answer code
+                        console.log('answer text');
+                        sentResult = true;
+                        sendResponse({ result: 'answer text', x: msg.x, y: msg.y });
+                        return;
+                    }
+                }
+            }
+    
+            if (element.classList.contains('post-tag')) {
+                console.log('question-tag');
+                sentResult = true;
+                sendResponse({ result: 'question tag', x: msg.x, y: msg.y });
+                return;
+            }
+    
+            if (element.classList.contains('vote')) {
+                if (questionElement.contains(element)) {
+                    // question code
+                    console.log('question vote');
+                    sentResult = true;
+                    sendResponse({ result: 'question vote', x: msg.x, y: msg.y });
+                    return;
+                }
+                for (answer of answerElements) {
+                    if (answer.contains(element)) {
+                        // answer code
+                        console.log('answer vote');
+                        sentResult = true;
+                        sendResponse({ result: 'answer vote', x: msg.x, y: msg.y });
+                        return;
+                    }
+                }
+            }
+    
+            if (element.Id == 'question-header') {
+                console.log('question-title');
+                sentResult = true;
+                sendResponse({ result: 'question-title', x: msg.x, y: msg.y });
+                return;
+            }
+        }
+    }
+
+    if (!sentResult) {
+        sendResponse(null);
     }
 });
--- Extension Files/assets/js/main.js
+++ Extension Files/assets/js/main.js
@@ -40,87 +40,96 @@ function translateCoordinates(x, y) {
         return null;
     } else {
         // user is looking in the broswer viewport, return the translated coordinates
-        return { x: x - offsetX, y: y - offsetY };
+        return { x: x - offsetY, y: y - offsetX };
     }
 }
 
-function getSOElementResult(stringElements) {
+/*function getSOElementResult(stringElements) {
     var parser = new DOMParser();
-    var elements = parser.parseFromString(stringElements, 'text/html');
-    var elements = elements.getElementsByTagName('body');
+    //var elements = parser.parseFromString(stringElements, 'text/html');
+    //var elements = elements.getElementsByTagName('body');
+    var elements = [];
+    for (string of stringElements) {
+        var bodyElement = parser.parseFromString(string.element, 'text/html');
+        bodyElement = bodyElement.getElementsByTagName('body');
+        var element = bodyElement[0].children[string.index];
+        elements.push(element);
+    } 
 
     for (element of elements) {
-        if (element.tagName == 'CODE') {
-            if (this.questionElement.Body.contains(element)) {
-                // question code
-                console.log('question code');
-                return 'question code';
-            }
-            for (answer in this.Body.answerElements) {
-                if (answer.contains(element)) {
-                    // answer code
-                    console.log('answer code');
-                    return 'answer code';
+        if (element) {
+            if (element.tagName == 'CODE') {
+                if (this.questionElement[0].contains(element)) {
+                    // question code
+                    console.log('question code');
+                    return 'question code';
+                }
+                for (answer of this.answerElements) {
+                    if (answer[0].contains(element)) {
+                        // answer code
+                        console.log('answer code');
+                        return 'answer code';
+                    }
                 }
             }
-        }
-
-        if (element.tagName == 'IMG') {
-            if (this.questionElement.Body.contains(element)) {
-                // question code
-                console.log('question image');
-                return 'question image';
-            }
-            for (answer in this.Body.answerElements) {
-                if (answer.contains(element)) {
-                    // answer code
-                    console.log('answer image');
-                    return 'answer image';
+    
+            if (element.tagName == 'IMG') {
+                if (this.questionElement[0].contains(element)) {
+                    // question code
+                    console.log('question image');
+                    return 'question image';
+                }
+                for (answer of this.answerElements) {
+                    if (answer[0].contains(element)) {
+                        // answer code
+                        console.log('answer image');
+                        return 'answer image';
+                    }
                 }
             }
-        }
-
-        if (element.classList.contains('post-text')) {
-            if (this.questionElement.Body.contains(element)) {
-                // question code
-                console.log('question text');
-                return 'question text';
-            }
-            for (answer in this.Body.answerElements) {
-                if (answer.contains(element)) {
-                    // answer code
-                    console.log('answer text');
-                    return 'answer text';
+    
+            if (element.classList.contains('post-text')) {
+                if (this.questionElement[0].contains(element)) {
+                    // question code
+                    console.log('question text');
+                    return 'question text';
+                }
+                for (answer of this.answerElements) {
+                    if (answer[0].contains(element)) {
+                        // answer code
+                        console.log('answer text');
+                        return 'answer text';
+                    }
                 }
             }
-        }
-
-        if (element.classList.contains('post-tag')) {
-            console.log('question-tag');
-            return 'question tag';
-        }
-
-        if (element.classList.contains('vote')) {
-            if (this.questionElement.Body.contains(element)) {
-                // question code
-                console.log('question vote');
-                return 'question vote';
+    
+            if (element.classList.contains('post-tag')) {
+                console.log('question-tag');
+                return 'question tag';
             }
-            for (answer in this.Body.answerElements) {
-                if (answer.contains(element)) {
-                    // answer code
-                    console.log('answer vote');
-                    return 'answer vote';
+    
+            if (element.classList.contains('vote')) {
+                if (this.questionElement[0].contains(element)) {
+                    // question code
+                    console.log('question vote');
+                    return 'question vote';
+                }
+                for (answer of this.answerElements) {
+                    if (answer[0].contains(element)) {
+                        // answer code
+                        console.log('answer vote');
+                        return 'answer vote';
+                    }
                 }
             }
-        }
-
-        if (element.Id == 'question-header') {
-            console.log('question-title');
-            return 'question-title';
+    
+            if (element.Id == 'question-header') {
+                console.log('question-title');
+                return 'question-title';
+            }
         }
     }
-}
+}*/
 
 function getBZElementResult(elements) {
     for (element of elements) {
@@ -142,24 +151,36 @@ function getBZElementResult(elements) {
    }
 }
 
-function receiveQuestion(questionElement) {
+/*function receiveQuestion(questionElement) {
     var parser = new DOMParser();
-    this.jQuestionElement = $.parseHTML(questionElement);
     this.questionElement = parser.parseFromString(questionElement, 'text/html');
     this.questionElement = this.questionElement.getElementsByTagName('body');
 
-    console.log(this.jQuestionElement);
     console.log(this.questionElement);
 }
 
 function receiveAnswers(answerElements) {
     var parser = new DOMParser();
-    this.jAnswerElements = $.parseHTML(answerElements);
-    this.answerElements = parser.parseFromString(answerElements, 'text/html');
-    this.answerElements = this.answerElements.getElementsByTagName('body');
+    //this.jAnswerElements = $.parseHTML(answerElements);
+    //this.answerElements = parser.parseFromString(answerElements, 'text/html');
+    //this.answerElements = this.answerElements.getElementsByTagName('body');
+    var elements = [];
+    for (string of answerElements) {
+        var element = parser.parseFromString(string, 'text/html');
+        element = element.getElementsByTagName('body');
+        elements.push(element);
+    }
+    
+    this.answerElements = elements;
 
-    console.log(this.jAnswerElements);
     console.log(this.answerElements);
+}*/
+
+function printSOResults(response) {
+    if (response) {
+        var printString = 'x: ' + response.x + ', y: ' + response.y + ', result: ' + response.result;
+        console.log(printString);
+    }
 }
 
 // establish the websocket connection
@@ -182,8 +203,6 @@ chrome.browserAction.onClicked.addListener(function (tab) {
         this.browserY = result[0];
     }.bind(this));
 
-    //chrome.tabs.executeScript({ 'file': '/assets/js/getSOQuestions.js' });
-
     var queryInfo = {
         active: true,
         currentWindow: true
@@ -193,12 +212,6 @@ chrome.browserAction.onClicked.addListener(function (tab) {
     console.log(url);
 
     var urlRegex = /^https?:\/\/(?:[^./?#]+\.)?stackoverflow\.com/;
-
-    if (urlRegex.test(url)) {
-        // ...if it matches, send a message specifying a callback too
-        chrome.tabs.sendMessage(id, { text: 'get_question' }, this.receiveQuestion);
-        chrome.tabs.sendMessage(id, { text: 'get_answers'  }, this.receiveAnswers );
-    }
 
     var websocket = new WebSocket('ws://localhost:7007');
 
@@ -226,10 +239,10 @@ chrome.browserAction.onClicked.addListener(function (tab) {
             // user is looking in the html viewport
             // need to check which website the user is looking at
             chrome.tabs.query({ 'active': true }, function (tabs) {
-                var url = tabs[0].url;
-                this.currentTab = tabs[0];
+                var url = tab.url;
+                this.currentTab = tab;
                 if (url.includes('stackoverflow.com/questions/')) {
-                    chrome.tabs.sendMessage(id, { text: 'get_so_coordinate', x: x, y: y  }, this.getSOElementResult );
+                    chrome.tabs.sendMessage(id, { text: 'get_so_coordinate', x: coords.x, y: coords.y  }, this.printSOResults );
                 }
                 if (url.includes('bugzilla')) { // NOTE: This include may be incorect, will need to do some more research
                     
