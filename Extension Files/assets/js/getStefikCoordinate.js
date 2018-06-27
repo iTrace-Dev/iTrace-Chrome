a13878be0a071cb61ce3399fc067cb83989cf0b7
Ashwin Mishra <amishra01@student.ysu.edu>
2018-06-27 04:46:44 -0400

    Can now identify which word was looked at in a webpage.

--- Extension Files/assets/js/getStefikCoordinate.js
+++ Extension Files/assets/js/getStefikCoordinate.js
@@ -2,6 +2,42 @@ chrome.runtime.onMessage.addListener(function (msg, sender, sendResponse) {
 	var elements = document.elementsFromPoint(msg.x, msg.y);
 
     var sentResult = false;
+	
+	function findToken(parentElt, x, y) {
+	console.log(parentElt.nodeName);
+    if (parentElt.nodeName !== '#text') {
+        console.log('didn\'t look on text node');
+        return null;
+    }
+    var range = document.createRange();
+    var words = parentElt.textContent.split(' ');
+    var start = 0;
+    var end = 0;
+    for (var i = 0; i < words.length; i++) {
+        var word = words[i];
+        end = start+word.length;
+        range.setStart(parentElt, start);
+        range.setEnd(parentElt, end);
+        // not getBoundingClientRect as word could wrap
+        var rects = range.getClientRects();
+        var clickedRect = isGazeInRects(rects);
+        if (clickedRect) {
+            return [word, start, clickedRect];
+        }
+        start = end + 1;
+    }
+    
+    function isGazeInRects(rects) {
+        for (var i = 0; i < rects.length; ++i) {
+            var r = rects[i]
+            if (r.left<x && r.right>x && r.top<y && r.bottom>y) {            
+                return r;
+            }
+        }
+        return false;
+    }
+    return null;
+	}
     for (element of elements) {
 		if (element.classList.contains('container-fluid')){
 			console.log('task info');
@@ -10,10 +46,17 @@ chrome.runtime.onMessage.addListener(function (msg, sender, sendResponse) {
 			return;
 		}
 		if (element.classList.contains('sampleline')){
-			console.log('code sample');
-			sentResult = true;
-			sendResponse({ result: 'code sample', x: msg.x, y: msg.y, time: msg.time, tagname: element.tagName, id: element.id, url: msg.url });
-			return;
+			console.log(element.tagName.nodeName);
+			var gazed = findToken(element.childNodes[0], msg.x, msg.y);	
+			if (gazed) {
+				var word = gazed[0];
+				var start = gazed[1]; //offset
+				var r = gazed[2];
+				console.log('code sample');
+				sentResult = true;
+				sendResponse({ result: word, x: msg.x, y: msg.y, time: msg.time, tagname: element.tagName, id: element.id, url: msg.url });
+				return;
+			}
 		}
 		if (element.classList.contains('inputline')){
 			console.log('solution area');
