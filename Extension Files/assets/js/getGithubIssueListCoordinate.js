1c60ebc469b38a2cbd4d353024190a1b8f15b15d
erohrs2 <elijahhrohrs@gmail.com>
2025-11-17 01:28:24 -0600

    Updated Stack Overflow and GitHub Issue Content Scripts

--- Extension Files/assets/js/getGithubIssueListCoordinate.js
+++ Extension Files/assets/js/getGithubIssueListCoordinate.js
@@ -1,18 +1,18 @@
 /****************************************************************************************************************************
  ****************************
  * @file FILE.EXT
- * 
+ *
  * @copyright (C) 2022 i-trace.org
- * 
+ *
  * This file is part of iTrace Infrastructure http://www.i-trace.org/.
  * iTrace Infrastructure is free software: you can redistribute it and/or modify it under the terms of the GNU General Public
  * License as published by the Free Software Foundation, either version 3 of the License, or (at your option) any later
  * version.
- * 
+ *
  * iTrace Infrastructure is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even the
  * implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public License
  * for more details.
- * 
+ *
  * You should have received a copy of the GNU General Public License along with iTrace Infrastructure. If not, see
  * https://www.gnu.org/licenses/.
  ************************************************************************************************************************
@@ -27,93 +27,130 @@ chrome.runtime.onMessage.addListener(function (msg, sender, sendResponse) {
     let sentResult = false;
     for (element of elements) {
         console.log("looping");
-        if (element.classList.contains('btn-link') && element.innerHTML.includes('Open') && element.tagName === 'A') {
-            console.log("Number of issues open");
-            const numberOpen = element.innerHTML;
+        if (element.tagName === 'A' && element.querySelector('div')?.textContent.trim() === 'Open') {
+            const numberOpen = element.querySelector('span[aria-hidden="true"]')?.textContent.trim();
+            console.log('Number of Open issues:', numberOpen);
             sentResult = true;
-            sendResponse({ result: `NumIssuesOpen-${numberOpen}`, x: msg.x, y: msg.y, time: msg.time, id: element.id, url: msg.url });
+            sendResponse({
+                result: `NumIssuesOpen-${numberOpen}`,
+                x: msg.x,
+                y: msg.y,
+                time: msg.time,
+                id: element.id,
+                url: msg.url
+            });
             return;
         }
-        if (element.classList.contains('social-count') && element.tagName === 'A') {
-            const ariaLabel = element.attributes.getNamedItem('aria-label').value;
-            if (ariaLabel.includes('forked')) {
-                console.log('Number of users who have forked repository');
-                const numberForked = element.innerHTML;
-                sentResult = true;
-                sendResponse({ result: `NumOfForked-${numberForked}`, x: msg.x, y: msg.y, time: msg.time, id: element.id, url: msg.url });
-                return;
+        if (
+            element.tagName === "A" &&
+            element.hasAttribute("data-hovercard-type")
+        ) {
+            const type = element.getAttribute("data-hovercard-type");
 
-            } else if (ariaLabel.includes('watching')) {
-                console.log('Number of users who are watching repository');
-                const numberWatching = element.innerHTML;
-                sentResult = true;
-                sendResponse({ result: `NumOfWatching- ${numberWatching}`, x: msg.x, y: msg.y, time: msg.time, id: element.id, url: msg.url });
-                return;
-            } else if (ariaLabel.includes('starred')) {
-                console.log('Number of users who starred repository');
-                const numberStarred= element.innerHTML;
-                sentResult = true;
-                sendResponse({ result: `NumOfStarred-${numberStarred}`, x: msg.x, y: msg.y, time: msg.time, id: element.id, url: msg.url });
-                return;
-            }
-        }
-        if (element.classList.contains('url') && element.classList.contains('fn') && element.tagName === 'A') {
-            if (!element.attributes.getNamedItem('data-hovercard-type')) {
-                return;
-            } 
-            if (element.attributes.getNamedItem('data-hovercard-type') === 'organization') {
+            if (type === "organization") {
                 console.log("Project/Organization Name");
-                const organization = element.innerHTML;
+
+                const organization = element.textContent.trim();
+
                 sentResult = true;
-                sendResponse({ result: `ProjectName-${organization}`, x: msg.x, y: msg.y, time: msg.time, id: element.id, url: msg.url });
+                sendResponse({
+                    result: `ProjectName-${organization}`,
+                    x: msg.x,
+                    y: msg.y,
+                    time: msg.time,
+                    id: element.id,
+                    url: msg.url
+                });
                 return;
-            }
-        }
-        if (element.classList.contains('IssueLabel')) {
-            console.log('Issue label')
-            const labelTitle = element.innerHTML;
-            sentResult = true;
-            sendResponse({ result: `IssueLabel-${labelTitle}`, x: msg.x, y: msg.y, time: msg.time, id: element.id, url: msg.url });
-            return;
-        }
-        if (element.classList.contains('muted-link')) {
-            if (!element.attributes.getNamedItem('data-hovercard-type')) {
-                return;
-            }
-            const linkType = element.attributes.getNamedItem('data-hovercard-type').value 
-            if (linkType === 'user') {
+            }else if (type === 'user') {
                 console.log('user link')
                 const user = element.innerHTML;
                 sentResult = true;
-                sendResponse({ result: `Username-${user}`, x: msg.x, y: msg.y, time: msg.time, id: element.id, url: msg.url });
+                sendResponse({
+                    result: `Username-${user}`,
+                    x: msg.x,
+                    y: msg.y,
+                    time: msg.time,
+                    id: element.id,
+                    url: msg.url
+                });
                 return;
-            } else if (linkType === 'issue') {
+            } else if (type === 'issue') {
                 console.log('issue link')
                 const issue = element.innerHTML;
                 sentResult = true;
-                sendResponse({ result: `Issue-${issue}`, x: msg.x, y: msg.y, time: msg.time, id: element.id, url: msg.url });
+                sendResponse({
+                    result: `Issue-${issue}`,
+                    x: msg.x,
+                    y: msg.y,
+                    time: msg.time,
+                    id: element.id,
+                    url: msg.url
+                });
                 return;
-            } else if (linkType.includes
-                ("comments")) {
+            } else if (type.includes
+            ("comments")) {
                 const numberOfComments = element.attributes.getNamedItem('aria-label').value;
                 sentResult = true;
-                sendResponse({ result: `NumOfComments-${numberOfComments}`, x: msg.x, y: msg.y, time: msg.time, url: msg.url });
+                sendResponse({
+                    result: `NumOfComments-${numberOfComments}`,
+                    x: msg.x,
+                    y: msg.y,
+                    time: msg.time,
+                    url: msg.url
+                });
                 return;
             }
+        }
+
+        if (
+            element.tagName === "A" &&
+            (
+                element.href.includes("label%3A") ||
+                element.href.includes("/labels/")
+            )
+        ) {
+            console.log('Issue label')
+            const labelTitle = element.innerHTML;
+            sentResult = true;
+            sendResponse({
+                result: `IssueLabel-${labelTitle}`,
+                x: msg.x,
+                y: msg.y,
+                time: msg.time,
+                id: element.id,
+                url: msg.url
+            });
+            return;
         }
         if (element.tagName === 'RELATIVE-TIME') {
             console.log('Time open')
             const opened = element.innerHTML;
             const timestamp = element.attributes.getNamedItem('title').value;
             sentResult = true;
-            sendResponse({ result: `IssueOpened-${opened} on ${timestamp}`, x: msg.x, y: msg.y, time: msg.time, id: element.id, url: msg.url });
+            sendResponse({
+                result: `IssueOpened-${opened} on ${timestamp}`,
+                x: msg.x,
+                y: msg.y,
+                time: msg.time,
+                id: element.id,
+                url: msg.url
+            });
             return;
         }
-        if (element.classList.contains('task-progress-counts')) {
+        if (element.closest('[data-listview-component="trailing-badge"]') &&
+            /\d+\s*\/\s*\d+/.test(element.textContent)) {
             console.log('Task progress bar')
             const taskProgress = element.innerHTML;
             sentResult = true;
-            sendResponse({ result: `TaskCompletion-${taskProgress}`, x: msg.x, y: msg.y, time: msg.time, id: element.id, url: msg.url });
+            sendResponse({
+                result: `TaskCompletion-${taskProgress}`,
+                x: msg.x,
+                y: msg.y,
+                time: msg.time,
+                id: element.id,
+                url: msg.url
+            });
             return;
         }
     }
--- Extension Files/assets/js/getSearchCoordinate.js
+++ Extension Files/assets/js/getSearchCoordinate.js
@@ -1,18 +1,18 @@
 /****************************************************************************************************************************
  ****************************
  * @file FILE.EXT
- * 
+ *
  * @copyright (C) 2022 i-trace.org
- * 
+ *
  * This file is part of iTrace Infrastructure http://www.i-trace.org/.
  * iTrace Infrastructure is free software: you can redistribute it and/or modify it under the terms of the GNU General Public
  * License as published by the Free Software Foundation, either version 3 of the License, or (at your option) any later
  * version.
- * 
+ *
  * iTrace Infrastructure is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even the
  * implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public License
  * for more details.
- * 
+ *
  * You should have received a copy of the GNU General Public License along with iTrace Infrastructure. If not, see
  * https://www.gnu.org/licenses/.
  ************************************************************************************************************************
@@ -24,37 +24,61 @@ chrome.runtime.onMessage.addListener(function (msg, sender, sendResponse) {
     var elements = document.elementsFromPoint(msg.x, msg.y);
 
     var fixationElement = document.getElementById('fixationMover');
-	if(fixationElement == null) {
-		fixationElement = document.createElement("DIV");
-		fixationElement.setAttribute('id', 'fixationMover');
+    if (fixationElement == null) {
+        fixationElement = document.createElement("DIV");
+        fixationElement.setAttribute('id', 'fixationMover');
         fixationElement.setAttribute('style', 'border-radius:100%;border:5px red solid;height:30px;width:30px;position:fixed');
         document.body.appendChild(fixationElement);
     }
-    
+
     fixationElement.style.left = msg.x + "px";
     fixationElement.style.top = msg.y + "px";
 
     var sentResult = false;
-	// console.log('Hello');
+    // console.log('Hello');
     for (element of elements) {
-        if (element.classList.contains('question-summary search-result')){
-			console.log('question summary-search');
+        if (element.classList.contains('question-summary search-result')) {
+            console.log('question summary-search');
             sentResult = true;
-            sendResponse({ result: 'question summary-search', x: msg.x, y: msg.y, time: msg.time, tagname: element.tagName, id: element.id, url: msg.url });
+            sendResponse({
+                result: 'question summary-search',
+                x: msg.x,
+                y: msg.y,
+                time: msg.time,
+                tagname: element.tagName,
+                id: element.id,
+                url: msg.url
+            });
             return;
-		}
-		if (element.classList.contains('vote-count-post')){
-			console.log('question-vote count');
+        }
+        if (element.classList.contains('s-post-summary--stats-item-number')) {
+            console.log('question-vote count');
             sentResult = true;
-            sendResponse({ result: 'question vote count', x: msg.x, y: msg.y, time: msg.time, tagname: element.tagName, id: element.id, url: msg.url });
+            sendResponse({
+                result: 'question vote count',
+                x: msg.x,
+                y: msg.y,
+                time: msg.time,
+                tagname: element.tagName,
+                id: element.id,
+                url: msg.url
+            });
             return;
-		}
-		if (element.classList.contains('summary')){
-			console.log('summary');
+        }
+        if (element.classList.contains('s-post-summary--content-excerpt')) {
+            console.log('summary');
             sentResult = true;
-            sendResponse({ result: 'summary', x: msg.x, y: msg.y, time: msg.time, tagname: element.tagName, id: element.id, url: msg.url });
+            sendResponse({
+                result: 'summary',
+                x: msg.x,
+                y: msg.y,
+                time: msg.time,
+                tagname: element.tagName,
+                id: element.id,
+                url: msg.url
+            });
             return;
-		}
+        }
     }
 
     if (!sentResult) {
